452321bb10d1890f566147abbc1af0ada85222f5
Osama_Eid <OsamaEid1>
2024-10-07 13:27:28 +0300

    Push Styles Changes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,16 +2,26 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        layouts: "#000000",
+        secBoxesColor: "#F0F0F5",
+        hovers: "#00A9C3",
+        borders: "#E0E0E0",
+        shadows: "#D6D6D6",
+        secTextColor: "#333333",
+        notes: "#FF9800",
       },
+      borderRadius: {
+        main: "0.75rem",
+      },
+      textOverflow: {
+        unset: "unset"
+      }
     },
   },
   plugins: [],
